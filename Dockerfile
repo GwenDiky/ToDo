0a6a85f840b50dfcaae18672cde0fe6076ced737
Tamagil <tamagiltamagil@gmail.com>
2024-09-30 15:14:50 +0300

    Feature/Added psql db to settings

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="ВИОЛЕТТА"
+
+ENTRYPOINT ["top", "-b"]
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'accounts',
+
+    #3d party apps
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +78,27 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_system',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        "OPTIONS": {
+            "options": "-c client_encoding=utf8"
+        }
     }
 }
+
 
 
 # Password validation
