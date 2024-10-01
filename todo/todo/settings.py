8fb158bc4788dcdebbb9e1cdc8896b5cb6d97f63
Tamagil <tamagiltamagil@gmail.com>
2024-10-01 09:27:49 +0300

    feat:edited settings with .env

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': 'pgdb_new',
         'PORT': os.getenv('DB_PORT'),
         "OPTIONS": {
             "options": "-c client_encoding=utf8"
