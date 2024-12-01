466fe276e3527c189db477fd48d616f97509bb24
Tamagil <tamagiltamagil@gmail.com>
2024-12-01 20:07:27 +0300

    feat: made utils to work with routers instead of row sql

--- tasks/api/v1/utils.py
+++ tasks/api/v1/utils.py
@@ -1,10 +1,15 @@
+import httpx
 from asgiref.sync import sync_to_async
-from django.db import connections
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+API_URL = settings.API_URL
 
 
 @sync_to_async
 def get_email_of_user(user_id: int) -> str | None:
-    with connections["user_db"].cursor() as cursor:
-        cursor.execute("SELECT email FROM users WHERE id = %s", [user_id])
-        result = cursor.fetchone()
-        return result[0] or None
+    with httpx.Client() as client:
+        response = client.get(f"{API_URL}/{user_id}/email")
+        if response.status_code == 200:
+            return response.json()
+        raise ValidationError("User with such id doesn't exist")
