fb48798d7cf97c6f87f6d4cb0aa727085d577b25
Tamagil <tamagiltamagil@gmail.com>
2024-12-13 14:19:02 +0300

    feat: separated mixins

--- projects/viewsets.py
+++ projects/viewsets.py
@@ -1,21 +1,9 @@
-import logging
-
-from django.db.models.query import QuerySet
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters as drf_filters
 from rest_framework import viewsets
-
-from projects.api.v1 import filters, serializers
 from projects.models import Project
-from tasks.api.v1.utils import extract_file_info
-from todo.api.v1 import kafka_producer
-from todo.jwt_auth import IsAuthenticatedById, JWTAuthenticationCustom
+from projects.api.v1 import serializers
+from projects.api.v1.filters import ProjectFilter
+from todo.mixins import FileEventMixin
 from todo.api.v1.kafka_event_sender import KafkaEventSender
-
-from kafka.errors import KafkaError
-
-
-logger = logging.getLogger(__name__)
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all().order_by("-pk")
@@ -23,21 +11,20 @@ class ProjectViewSet(viewsets.ModelViewSet):
     lookup_field = "id"
 
     filter_backends = (DjangoFilterBackend, drf_filters.OrderingFilter)
-
-    filterset_class = filters.ProjectFilter
+    filterset_class = ProjectFilter
     ordering_fields = ["title", "created_at"]
 
     authentication_classes = [JWTAuthenticationCustom]
     permission_classes = [IsAuthenticatedById]
 
-    def get_queryset(self) -> QuerySet[Project]:
+    def get_queryset(self):
         return Project.objects.filter(
             tasks__user_id=self.request.user
         ).distinct()
 
     def perform_create(self, serializer):
         project = serializer.save()
-        self.send_file_event(event_type="upload_file", project=project)
+        self.send_file_event(event_type="upload_file", instance=project)
         return project
 
     def perform_update(self, serializer):
@@ -47,14 +34,14 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
         if previous_attachment != updated_project.attachment_url:
             self.send_file_event(
-                event_type="update_file", project=updated_project
+                event_type="update_file", instance=updated_project
             )
 
         return updated_project
 
     def perform_destroy(self, instance):
         if instance.attachment_url:
-            self.send_file_event(event_type="delete_file", project=instance)
+            self.send_file_event(event_type="delete_file", instance=instance)
         super().perform_destroy(instance)
 
     def send_file_event(self, event_type, project):
@@ -68,6 +55,4 @@ class ProjectViewSet(viewsets.ModelViewSet):
             KafkaEventSender.send_attachment_event(
                 event_type=event_type, instance=file_instance
             )
-            logger.info(
-                f"Sent event '{event_type}' for file {project.attachment_url}"
-            )
+            logger.info(f"Sent event '{event_type}' for file {project.attachment_url}")
